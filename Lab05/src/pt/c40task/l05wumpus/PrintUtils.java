c3208bfbfaad0a3a0a80c531b28b67805013a27d
Gabriel Mikio <gmikioas@gmail.com>
2022-05-19 05:53:45 -0300

    Update PrintUtils.java

--- Lab05/src/pt/c40task/l05wumpus/PrintUtils.java
+++ Lab05/src/pt/c40task/l05wumpus/PrintUtils.java
@@ -1,5 +1,23 @@
 package pt.c40task.l05wumpus;
 
 public class PrintUtils {
-
+	// Possibilidade de melhoria: nao deixar fixas as dimensoes 4x4 da caverna
+	public static void caveState(char[][] caverna) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++)
+               System.out.print(caverna[i][j] + ((j != 3) ? ", " : "\n"));         }
+    }
+	
+	public static void displayName(String playerName) {
+		System.out.println("Player: " + playerName);
+	}
+	
+	public static void displayScore(int score) {
+		System.out.println("Score: " + score);
+	}
+	
+	public static void displayMessage(String message) {
+		System.out.println(message);
+	}
+	
 }
