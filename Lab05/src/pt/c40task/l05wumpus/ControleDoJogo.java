ec6f39d480f475a8f64267692ddbb4b200d25625
Gabriel Mikio <gmikioas@gmail.com>
2022-05-19 00:43:52 -0300

    Atualização dos arquivos nesta branch

--- Lab05/src/pt/c40task/l05wumpus/ControleDoJogo.java
+++ Lab05/src/pt/c40task/l05wumpus/ControleDoJogo.java
@@ -1,5 +1,7 @@
 package pt.c40task.l05wumpus;
 
+// Uma Classe de Controle do Jogo. Essa classe deve controlar os movimentos e pontuacao do jogo.
+
 public class ControleDoJogo {
 	public ControleDoJogo() {
 		
